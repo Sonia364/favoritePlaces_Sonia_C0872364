b5ecca496d36052b82ac8a81ba7b8171c7018bf8
Sonia364 <c0872364@mylambton.ca>
2023-01-24 18:06:19 -0500

    Initial Commit

---
+++ favoritePlaces_Sonia_C0872364/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favoritePlaces_Sonia_C0872364
+//
+//  Created by Sonia Nain on 2023-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
